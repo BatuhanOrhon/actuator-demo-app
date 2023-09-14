3429c8fb9a5e842bdb99541f95966356c57388da
BatuhanOrhon <135467282+BatuhanOrhon@users.noreply.github.com>
2023-09-14 14:59:25 +0300

    Add files via upload

---
+++ src/test/java/com/actuator/example/actuatordemoapp/ActuatorDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actuator.example.actuatordemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
